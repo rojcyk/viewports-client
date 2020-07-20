f2ebb06e447fa80cb369efffd68b625977d683bf
rojcyk <denis@rojcyk.com>
2020-07-20 21:43:46 +0200

    Creating a listener that checks for incoming updates from the client

---
+++ src/code/updateListener.ts
@@ -0,0 +1,14 @@
+import io from 'figmaio/code'
+import { DATA_UPDATE } from '../constants/events'
+import { LAST_CHECK, VIEWPORTS } from '../constants/storageProps'
+
+export default async (): Promise<void> => {
+  io.on(
+    DATA_UPDATE,
+    async (data): Promise<void> => {
+      console.log(`[Viewports] Recieved the updated data and saving them to Figma`)
+      await figma.clientStorage.setAsync(VIEWPORTS, data.viewports)
+      await figma.clientStorage.setAsync(LAST_CHECK, data.lastCheck)
+    },
+  )
+}
