74e5214390771e766cf6cea695960aaed501984a
rojcyk <denis@rojcyk.com>
2020-07-24 01:30:35 +0200

    Adding platform component

---
+++ src/app/platform/index.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react'
+
+import { Section } from '../section'
+import { PlatformToggle } from './platformToggle'
+import { Viewport } from '../viewport/index'
+// import { PlatformSelection } from './platformSelection'
+
+interface PlatformProps {
+  trigger: Function
+  displayTrigger: Function
+  platform: any
+  data: Client.Viewport[]
+  expanded?: boolean
+}
+
+export class Platform extends Section<PlatformProps> {
+  public renderToggle(): React.ReactNode {
+    return (
+      <PlatformToggle
+        label={this.props.platform}
+        expanded={this.state.expanded}
+        customAction={this.toogleExpand}
+        trigger={this.props.trigger}
+        platform={this.props.platform}
+      />
+    )
+  }
+
+  public renderContent(): React.ReactNode {
+    const list = this.props.data.map((viewport: Client.Viewport, i: number) => {
+      return (
+        <Viewport 
+          share={viewport.share}
+          width={viewport.display.width}
+          height={viewport.display.height}
+          trigger={this.props.displayTrigger}
+          key={i}
+          osVisible={true}
+        />
+      )
+    })
+
+    return (
+      <div>{list}</div>
+    )
+  }
+}
