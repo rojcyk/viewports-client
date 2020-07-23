abaaf8c5f9db4a9d906f72c79e64808fcc5fc224
rojcyk <denis@rojcyk.com>
2020-07-23 22:31:17 +0200

    Adding icons

---
+++ src/app/icons/arrow.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+import { animationSpeed } from '../../constants/tokens'
+
+const ArrowStyles = styled.svg`
+  margin-right: 12px;
+  transition: transform ${animationSpeed}ms ease-out;
+`
+
+interface ArrowProps {
+  expanded?: boolean
+}
+
+export const Arrow: React.StatelessComponent<ArrowProps> = (
+  props: ArrowProps,
+) => {
+  let transform
+
+  if (props.expanded) transform = 'rotate(90deg)'
+  else transform = 'rotate(0deg)'
+
+  return (
+    <ArrowStyles
+      width="4"
+      height="6"
+      viewBox="0 0 4 6"
+      style={{ transform: transform }}
+    >
+      <path d="M0.125 6L0.125 0L3.875 3L0.125 6Z" />
+    </ArrowStyles>
+  )
+}
