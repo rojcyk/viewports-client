9e22ad6ffc6882ff759eb8226a7b0db6a40cc4f5
rojcyk <denis@rojcyk.com>
2020-07-23 22:31:10 +0200

    Adding basic tokens

---
+++ src/constants/tokens.ts
@@ -0,0 +1,15 @@
+
+export const animationSpeed = 300
+
+export const colors = {
+  bg: {
+    primary: '#FFFFFF',
+    secondary: '#FBFBFB',
+    tertiary: '#F3F3F3',
+  },
+  cta: '#18A0FB',
+  ink: {
+    primary: '#333333',
+    secondary: '#757575',
+  },
+}
