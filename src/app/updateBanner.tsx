dc488cd4beec880bce7c7ce77612808271e4da75
rojcyk <denis@rojcyk.com>
2020-07-24 01:29:16 +0200

    Fixed animation, it now works correctly.

--- src/app/updateBanner.tsx
+++ src/app/updateBanner.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import styled from 'styled-components'
-import { animationSpeed } from '../constants/tokens'
 
 const height = 32
+const transitionSpeed = '0.6s'
 
 export interface UpdateProps {
   update: string
@@ -16,8 +16,8 @@ const UpdateWrapper = styled.div`
   position: fixed;
   left: 0;
   z-index: 100;
-  transition-duration: ${animationSpeed};
-  background-color: red;
+  transition: all;
+  transition-duration: ${transitionSpeed};
   transition-timing-function: cubic-bezier(0.33, 1, 0.68, 1);
 
   bottom: ${(props: UpdateProps): string => {
@@ -40,7 +40,8 @@ const UpdateBannerStyle = styled.div`
   font-weight: 600;
   padding: 8px;
   z-index: 100;
-  transition-duration: ${animationSpeed};
+  transition: all;
+  transition-duration: ${transitionSpeed};
   border-radius: 3px;
 
   background-color: ${(props: UpdateProps): string => {
