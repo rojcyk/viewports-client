e96eeb6d0a911b4fc242da5433eda4ef073974af
rojcyk <denis@rojcyk.com>
2020-07-28 00:18:28 +0200

    Adding a couple of basic icons for the promotion

---
+++ src/app/icons/heart.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+export const Heart: React.StatelessComponent = () => {
+  return (
+    <svg width="22" height="20" viewBox="0 0 22 20">
+      <g filter="url(#filter0_i)">
+        <path fill="#E30A17" d="M19.982 11.104A6.384 6.384 0 0022 6.447C22 2.887 19.072 0 15.46 0a6.57 6.57 0 00-4.504 1.771A6.571 6.571 0 006.541.083C2.93.083 0 2.969 0 6.529a6.38 6.38 0 001.933 4.576L10.958 20l9.025-8.895-.001-.001z" /></g><path fill="#E30A17" d="M19.639 10.74l-.377.356.01.01-8.314 8.192-8.674-8.55h0A5.88 5.88 0 01.5 6.528C.5 3.252 3.198.583 6.541.583c1.574 0 3.005.59 4.08 1.56l.34.307.336-.314A6.07 6.07 0 0115.459.5C18.802.5 21.5 3.17 21.5 6.447c0 1.686-.713 3.21-1.861 4.293z" stroke="#992929"/><defs><filter id="filter0_i" x="0" y="0" width="22" height="20" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/><feOffset dy="2"/><feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/><feColorMatrix values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.44 0"/><feBlend in2="shape" result="effect1_innerShadow"/></filter></defs>
+    </svg>
+  )
+}
---
+++ src/app/icons/money.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+export const Money: React.StatelessComponent = () => {
+  return (
+    <svg width="22" height="22" viewBox="0 0 22 22">
+      <ellipse cx="10.177" cy="10.0851" rx="3.26827" ry="3.26827" transform="rotate(-5.49003 10.177 10.0851)" fill="#39B54A"/>
+<rect x="12.9424" y="0.671599" width="10.4417" height="17.7045" rx="1.5" transform="rotate(45.634 12.9424 0.671599)" fill="#036933"/>
+<ellipse cx="10.4937" cy="10.1316" rx="3.26827" ry="3.26827" transform="rotate(30.634 10.4937 10.1316)" fill="#39B54A"/>
+<rect x="12.9424" y="0.671599" width="10.4417" height="17.7045" rx="1.5" transform="rotate(45.634 12.9424 0.671599)" stroke="#065A04" fill="#009245"/>
+<rect x="10.1693" y="0.316987" width="10.4417" height="17.7045" rx="1.5" transform="rotate(30 10.1693 0.316987)" fill="#027639"/>
+<ellipse cx="10.3606" cy="10.0868" rx="3.26827" ry="3.26827" transform="rotate(15 10.3606 10.0868)" fill="#39B54A"/>
+<rect x="10.1693" y="0.316987" width="10.4417" height="17.7045" rx="1.5" transform="rotate(30 10.1693 0.316987)" stroke="#065A04" fill="#009245"/>
+<rect x="6.57801" y="1.00043" width="10.4417" height="17.7045" rx="1.5" transform="rotate(9.50997 6.57801 1.00043)" fill="#009245"/>
+<rect fill="#009245" x="6.57801" y="1.00043" width="10.4417" height="17.7045" rx="1.5" transform="rotate(9.50997 6.57801 1.00043)" stroke="#065A04"/>
+    </svg>
+  )
+}
