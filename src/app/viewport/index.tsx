668a8fd34129bb7e969820ce926ac3cf551695dd
Denis Rojcyk <denis@rojcyk.com>
2020-08-02 15:06:30 +0200

    Adding more iOS devices, returning android for everything else. Will add platform differentiation in the future

--- src/app/viewport/index.tsx
+++ src/app/viewport/index.tsx
@@ -42,18 +42,12 @@ export const selectOS = (width: number, height: number): number => {
   switch (dimensions) {
     case '320x568':
     case '375x667':
-    case '414x736':
-      return 1
-    case '360x640':
     case '375x812':
-    case '412x732':
-    case '412x824':
-    case '412x846':
-    case '412x847':
+    case '414x736':
     case '414x896':
-      return 2
+      return 1
     default:
-      return 0
+      return 2
   }
 }
 
